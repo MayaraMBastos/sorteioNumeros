ce7661fce75afafceeddc35c9927a67786808527
Mayara Martinello Bastos <maybastos2021@gmail.com>
2023-09-16 23:09:07 -0300

    imports desnecessarios

--- src/main/java/avaliacao/sorteio/mayara/sorteioNumeros/Controller/C_Resultado.java
+++ src/main/java/avaliacao/sorteio/mayara/sorteioNumeros/Controller/C_Resultado.java
@@ -7,9 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.time.LocalDateTime;
+
+
 import java.util.Date;
 
 @Controller
