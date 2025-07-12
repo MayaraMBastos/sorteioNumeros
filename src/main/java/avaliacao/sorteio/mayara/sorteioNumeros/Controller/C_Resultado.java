fd516dc6cc9f5c1756b26ad6642c2318d74be9ae
Mayara Martinello Bastos <111710970+MayaraMBastos@users.noreply.github.com>
2025-07-12 09:05:06 -0300

    Update C_Resultado.java
    fix: rota do resultado

--- src/main/java/avaliacao/sorteio/mayara/sorteioNumeros/Controller/C_Resultado.java
+++ src/main/java/avaliacao/sorteio/mayara/sorteioNumeros/Controller/C_Resultado.java
@@ -14,26 +14,27 @@ import java.util.Date;
 @Controller
 public class C_Resultado {
 
-    @GetMapping("resultado")
+    @GetMapping("/resultado")
     public String pagResultado() {
-        return "resultado";
+        return "resultado"; // Renderiza templates/resultado.html
     }
 
     @PostMapping("/")
     public String listaNumeros(@RequestParam("quantidade") int quantidade,
-            @RequestParam("inicio") int inicio,
-            @RequestParam("fim") int fim,
-            @RequestParam(name = "ordemCres", required = false, defaultValue = "false") boolean ordemCres,
-            @RequestParam(name = "repeticao", required = false, defaultValue = "false") boolean repeticao,
-            Model model, RedirectAttributes redirectAttributes, HttpSession session) {
+                                @RequestParam("inicio") int inicio,
+                                @RequestParam("fim") int fim,
+                                @RequestParam(name = "ordemCres", required = false, defaultValue = "false") boolean ordemCres,
+                                @RequestParam(name = "repeticao", required = false, defaultValue = "false") boolean repeticao,
+                                Model model,
+                                HttpSession session) {
 
         int[] vResultado = S_Sortear.sortearNumeros(quantidade, inicio, fim, ordemCres, repeticao);
+
         model.addAttribute("numeros", vResultado);
         model.addAttribute("quantidade", quantidade);
         model.addAttribute("dataHora", new Date().toLocaleString());
         model.addAttribute("faixaNumeros", inicio + " e " + fim);
-        return "/resultado";
 
+        return "resultado"; // Corrigido: sem barra no início
     }
-
 }
